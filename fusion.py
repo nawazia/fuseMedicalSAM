0d6e06c7f4aa04ef2beec2d06cae03dbefdf792c
Ibrahim Nawaz <ibrahim@resmonics.onmicrosoft.com>
2025-08-04 01:52:42 +0100

    Bug fix: ImageLevelFusion

--- fusion.py
+++ fusion.py
@@ -4,7 +4,7 @@ import numpy as np
 def ImageLevelFusion(models, mask_path, mask_filename):
     min_loss = np.inf
     for model_name in models:
-        mask_path = os.path.join(mask_path, model_name, mask_filename[:-4] + "_mask_logits.npz")
+        mask_path = os.path.join(mask_path, model_name, os.path.basename(mask_filename)[:-4] + "_mask_logits.npz")
         data = np.load(mask_path)
 
     return data
