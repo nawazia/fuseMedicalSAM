318e46f04a09a723917959c7f2402cb4f5d8693e
Ibrahim Nawaz <ibrahim@resmonics.onmicrosoft.com>
2025-08-05 14:59:28 +0100

    ADded print

--- fuseSAM.py
+++ fuseSAM.py
@@ -60,6 +60,7 @@ def knowledge_externalization(models : list,
 
         client = storage.Client()
         bucket = client.bucket(bucket_name)
+        print("bucket_name:", bucket_name)
     else:
         # For local paths, ensure the base directory exists
         os.makedirs(save_path, exist_ok=True)
@@ -73,6 +74,7 @@ def knowledge_externalization(models : list,
             # List all objects under the model's prefix and check the count
             blob_list = list(bucket.list_blobs(prefix=model_blob_prefix))
             print(model_blob_prefix)
+            print(blob_list)
             if len(blob_list) == num_images:
                 print(f"All mask logits for {model_name} already exist, skipping model...")
                 continue
